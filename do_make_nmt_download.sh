805562d74ff21ffbc04f22c7e1adac32ff305fff
David Liebman <radiodee1@gmail.com>
2020-12-16 13:42:00 -0500

    raw dir

--- do_make_nmt_download.sh
+++ do_make_nmt_download.sh
@@ -5,6 +5,9 @@ pip install -e .
 pip install -r requirements.opt.txt
 
 cd ..
+mkdir raw
+cd raw
+
 wget https://s3.amazonaws.com/opennmt-models/sum_transformer_model_acc_57.25_ppl_9.22_e16.pt
 
-
+cd ..
