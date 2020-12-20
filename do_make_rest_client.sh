511c7b828693e7f55523cde121cbe27980b762fc
David Liebman <radiodee1@gmail.com>
2020-12-20 14:18:23 -0500

    try rest server

--- do_make_rest_client.sh
+++ do_make_rest_client.sh
@@ -1,8 +1,7 @@
 export HOST="127.0.0.1"
 export PORT=5000
-export URL_ROOT="/"
-export CONFIG="../json/conf.json"
+export URL_ROOT="/OpenNMT-py"
 
 curl -i -X POST -H "Content-Type: application/json" \
-    -d '[{"src": "this is a test for model 0", "id": 0}]' \
+    -d '[{"src": "this is a test for model 0", "id": 100}]' \
     http://$HOST:$PORT$URL_ROOT/translate
--- do_make_rest_server.sh
+++ do_make_rest_server.sh
@@ -1,7 +1,7 @@
 export IP="127.0.0.1"
 export PORT=5000
-export URL_ROOT="../OpenNMT-py"
-export CONFIG="../json/server_conf.json"
+export URL_ROOT="/OpenNMT-py"
+export CONFIG="json/server_conf.json"
 
 # NOTE that these parameters are optionnal
 # here, we explicitely set to default values
