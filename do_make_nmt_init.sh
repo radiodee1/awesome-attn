1f3d401aa425d0b8f11e072d5ec0720e3e0eecf8
David Liebman <radiodee1@gmail.com>
2020-12-20 15:54:00 -0500

    tokenizer

--- do_make_nmt_init.sh
+++ do_make_nmt_init.sh
@@ -7,6 +7,14 @@ python setup.py install
 pip install -r requirements.opt.txt
 cd ..
 
+cd Tokenizer
+git submodule update --init
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
 pip install -r requirements.amd64.txt
 
 #cd ..
@@ -14,6 +22,6 @@ mkdir raw
 mkdir data
 cd raw
 
-wget https://s3.amazonaws.com/opennmt-models/sum_transformer_model_acc_57.25_ppl_9.22_e16.pt
+#wget https://s3.amazonaws.com/opennmt-models/sum_transformer_model_acc_57.25_ppl_9.22_e16.pt
 
 cd ..
--- do_make_rest_client.sh
+++ do_make_rest_client.sh
@@ -4,4 +4,7 @@ export URL_ROOT="/OpenNMT-py"
 
 curl -i -X POST -H "Content-Type: application/json" \
     -d '[{"src": "this is a test for model 0", "id": 100}]' \
-    http://$HOST:$PORT$URL_ROOT/translate
+    http://$HOST:$PORT$URL_ROOT/translate | Tokenizer/cli/detokenize
+ 
+
+
