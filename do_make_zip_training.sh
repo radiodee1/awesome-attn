36c55eb8fdc246107ffe3e6c225427d065c5f694
David Liebman <radiodee1@gmail.com>
2020-12-22 15:06:46 -0500

    prep and zip

--- do_make_zip_training.sh
+++ do_make_zip_training.sh
@@ -5,7 +5,7 @@ zip transformer_bpe.zip data/bpe/valid.src data/bpe/valid.tgt
 zip transformer_bpe.zip data/bpe/test.src data/bpe/test.tgt
 
 echo "raw files"
-zip transformer_raw.zip data/bpe-model-32k
+#zip transformer_raw.zip data/bpe-model-32k
 zip transformer_raw.zip data/train.from data/train.to
 zip transformer_raw.zip data/valid.from data/valid.to
 zip transformer_raw.zip data/test.from data/test.to
