3abb0cd1bce4db84627333014103649dff7c574c
David Liebman <radiodee1@gmail.com>
2020-12-22 14:45:24 -0500

    zip files

---
+++ do_make_zip_training.sh
@@ -0,0 +1,15 @@
+echo "bpe files"
+zip transformer_bpe.zip data/bpe/vocab.src data/bpe/bpe-model-32k
+zip transformer_bpe.zip data/bpe/train.src data/bpe/train.tgt
+zip transformer_bpe.zip data/bpe/valid.src data/bpe/valid.tgt
+zip transformer_bpe.zip data/bpe/test.src data/bpe/test.tgt
+
+echo "raw files"
+zip transformer_raw.zip data/bpe-model-32k
+zip transformer_raw.zip data/train.from data/train.to
+zip transformer_raw.zip data/valid.from data/valid.to
+zip transformer_raw.zip data/test.from data/test.to
+
+
+
+
