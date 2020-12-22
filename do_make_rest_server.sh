f5b13087528c20a4c60451311a5016e4739eed21
David Liebman <radiodee1@gmail.com>
2020-12-22 08:54:31 -0500

    json and server conf script

--- do_make_rest_server.sh
+++ do_make_rest_server.sh
@@ -1,8 +1,21 @@
 export IP="127.0.0.1"
 export PORT=5000
 export URL_ROOT="/OpenNMT-py"
-export CONFIG="json/server_conf.json"
+export CONFIG="json/server_conf_best.json"
 
-# NOTE that these parameters are optionnal
-# here, we explicitely set to default values
+echo $#
+echo "Enter number for 'best' saved checkpoint step."
+
+if [[ "1" -eq "$#" && "$1" != "best" ]] ; then
+    echo 'one input'
+    echo $1
+
+    if [[ -f data/transformer_movie_chat_step_$1.pt ]] ; then
+        rm data/transformer_movie_chat_step_best.pt
+        cp data/transformer_movie_chat_step_$1.pt data/transformer_movie_chat_step_best.pt
+    fi
+
+fi
+
+
 onmt_server --ip $IP --port $PORT --url_root $URL_ROOT --config $CONFIG
