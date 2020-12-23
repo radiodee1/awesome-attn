784c3397f47f3ac91b92e33203d9a6a0395c0dfb
David Liebman <radiodee1@gmail.com>
2020-12-23 13:26:53 -0500

    train_from option

--- do_launch_train.sh
+++ do_launch_train.sh
@@ -1,3 +1,13 @@
+echo $#
+
+if [[ "$#" == "1" && -f "$1"  ]] ; then
+    echo "train_from $1"
+    onmt_train --config yaml/train_config.yaml --train_from $1
+
+    exit
+fi
+
+
 onmt_train --config yaml/train_config.yaml
 
 
