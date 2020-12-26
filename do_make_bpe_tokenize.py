8819864256097e5fbbf3c7e4faabf1439cb6067d
David Liebman <radiodee1@gmail.com>
2020-12-26 09:34:36 -0500

    must test tokenize

--- do_make_bpe_tokenize.py
+++ do_make_bpe_tokenize.py
@@ -2,6 +2,16 @@
 
 import pyonmttok
 import os
+import sys
+
+if len(sys.argv) == 1: 
+    print('Enter name of file to convert as first parameter.')
+    exit()
+
+print(sys.argv[1])
+
+z = sys.argv[1]
+z = z.split('/')[-1]
 
 tokenizer = pyonmttok.Tokenizer(
         "aggressive", 
@@ -9,5 +19,11 @@ tokenizer = pyonmttok.Tokenizer(
         segment_numbers=True,
         bpe_model_path="./data/bpe/bpe-model-32k"
         )
+
+
+print("tokenize")
+tokenizer.tokenize_file(sys.argv[1],"./data/bpe/"+ z+ ".bpe.txt")
+
+print("File moved to ./data/bpe/" + z + '.bpe.txt')
 
 
