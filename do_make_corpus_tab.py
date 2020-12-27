fe13a8d973be3190cbf08c630bb1331fef8f7675
David Liebman <radiodee1@gmail.com>
2020-12-27 15:50:42 -0500

    generate times

--- do_make_corpus_tab.py
+++ do_make_corpus_tab.py
@@ -63,23 +63,23 @@ def string_from_date_info(y, month, day, h = 0, m = 0):
     return time + ", " + date 
 
 if __name__  == '__main__' :
-    print(food)
+    #print(food)
     now = datetime.datetime.now()
     time = now.strftime("%I:%M %p")
     date = now.strftime("%B %d, %Y")
     print(time+ ", " + date )
 
-    print(string_from_date_info(2020, 10, 12))
+    #print(string_from_date_info(2020, 10, 12))
 
     for i in [2012, 2020, 2021]: # year
-        for j in range(1, 12):
-            for k in range(0, 31):
+        for j in range(1, 12): # month
+            for k in range(0, 31): # day
                 for l in [0,12, 15]: # hour
                     for m in [0, 30, 45, 61]: # minutes
                         times += [string_from_date_info(i, j, k, h=l, m=m)]
 
     print("times generated", len(times))
-
+    #print(times)
 
 
 
