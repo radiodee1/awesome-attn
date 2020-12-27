55067e0d80d863284ebd47a3dcbd9c6c5b8775b0
David Liebman <radiodee1@gmail.com>
2020-12-27 14:34:02 -0500

    make extra corpus

---
+++ do_make_corpus_tab.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.6
+
+import argparse
+import os
+
+print("python3.6")
+
+names = [
+            'david',
+            'edward',
+            'bob',
+            'jane',
+            'kate',
+            'mary',
+            'john',
+            'paul',
+            'george'
+        ]
+
+places = [
+            'california',
+            'new york',
+            'washington',
+            'georgia',
+            'nevada',
+            'mississippi',
+            'florida'
+        ]
+
+times = []
+
+food = [
+            'ice cream',
+            'chili',
+            'ham',
+            'candy',
+            'chocolate'
+        ]
+
+color = [
+            'red',
+            'green',
+            'blue',
+            'yellow'
+        ]
+
+if __name__  == '__main__' :
+    print(food)
+
