b8e257b676c8cfe03732693401f3de44421e747e
David Liebman <radiodee1@gmail.com>
2020-12-29 16:12:25 -0500

    corpus extra

--- do_make_zip_training.sh
+++ do_make_zip_training.sh
@@ -3,13 +3,14 @@ zip transformer_bpe.zip data/bpe/vocab.src data/bpe/bpe-model-32k
 zip transformer_bpe.zip data/bpe/train.src data/bpe/train.tgt
 zip transformer_bpe.zip data/bpe/valid.src data/bpe/valid.tgt
 zip transformer_bpe.zip data/bpe/test.src data/bpe/test.tgt
+zip transformer_bpe.zip data/bpe/extra.src data/bpe/extra.tgt
 
 echo "raw files"
 #zip transformer_raw.zip data/bpe-model-32k
 zip transformer_raw.zip data/train.from data/train.to
 zip transformer_raw.zip data/valid.from data/valid.to
 zip transformer_raw.zip data/test.from data/test.to
-
+zip transformer_raw.zip data/extra.src data/extra.tgt
 
 
 
