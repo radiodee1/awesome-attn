dfd9d073a3eefd912fc7e593e7f754b1ec5f97c9
David Liebman <radiodee1@gmail.com>
2020-12-30 16:32:32 -0500

    exit sentencepiece learner

--- do_launch_prep.sh
+++ do_launch_prep.sh
@@ -12,4 +12,6 @@ fi
 
 onmt_build_vocab --config $CONFIG -bpe_model_path ./data/bpe/bpe-model-32k  -size 8000 #-n_sample 80000 
 
+#onmt_build_vocab --config $CONFIG -model_path ./data/mod/mod-model-32k  -size 8000 --overwrite True --src_vocab ./data/mod/vocab.src #-n_sample 80000 
+
 
