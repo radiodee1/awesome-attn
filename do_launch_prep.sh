81e5a575aaa70b330a5e2f751fbb80a29e52262c
David Liebman <radiodee1@gmail.com>
2020-12-30 10:05:37 -0500

    corpus extra -- build vocab stage

--- do_launch_prep.sh
+++ do_launch_prep.sh
@@ -1,5 +1,15 @@
 ./bpe_pyonmttok.py
 
-onmt_build_vocab --config yaml/train_config.yaml -n_sample 8000 
+CONFIG="yaml/train_config.yaml"
+CONFIG_EXTRA="yaml/train_config_extra.yaml"
+CORPUS_EXTRA="data/bpe/extra.src"
+
+if [[ -f "$CORPUS_EXTRA" ]] ; then
+    CONFIG=$CONFIG_EXTRA
+    echo "'extra' files detected..."
+fi
+
+
+onmt_build_vocab --config $CONFIG -n_sample 8000 
 
 
